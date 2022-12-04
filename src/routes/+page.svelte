2673324c45b8c5a16be4d1833ad78b42932113a6
Luna Simons <luna@bddvlpr.com>
2022-12-04 20:30:43 +0100

    fix: 🐛 leaflet map rendering issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,19 @@
 	import { LeafletMap, TileLayer } from 'svelte-leafletjs';
 </script>
 
-<div>
-	<LeafletMap
+<LeafletMap
+	options={{
+		center: [1.364917, 103.822872],
+		zoom: 11
+	}}
+>
+	<TileLayer
+		url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
 		options={{
-			center: [1.364917, 103.822872],
-			zoom: 11
+			minZoom: 0,
+			maxZoom: 20,
+			maxNativeZoom: 19,
+			attribution: '© OpenStreetMap contributors'
 		}}
-	>
-		<TileLayer
-			url={`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`}
-			options={{
-				minZoom: 0,
-				maxZoom: 20,
-				maxNativeZoom: 19,
-				attribution: '© OpenStreetMap contributors'
-			}}
-		/>
-	</LeafletMap>
-</div>
+	/>
+</LeafletMap>
